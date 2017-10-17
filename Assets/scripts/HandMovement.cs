06b2f41bd3c471a4ba3b0ff82ae638b2c61e9f27
bonattt <bonattt@rose-hulman.edu>
2017-10-16 23:48:03 -0400

    made a scene where you can (sort of) swing a wrench

---
+++ Assets/scripts/HandMovement.cs
@@ -0,0 +1,178 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandMovement : MonoBehaviour {
+
+    private enum HandState
+    {
+        Start,
+        Adjusting,
+        FinishedAdjusting,
+        Reversing,
+        Swinging
+    }
+
+    public float intervalMax = 30f;
+    public float intervalMin = 10f;
+
+    public float durationMax = 1;
+    public float durationMin = .5f;
+
+    public float xRotMax = 10;
+    public float xRotMin = 0;
+
+    public float yRotMax = 20;
+    public float yRotMin = 10;
+
+    public float zRotMax = 10;
+    public float zRotMin = 0;
+
+    public float swingRotSpeed = -360;
+    public float swingTransSpeed = 5f;
+    public float swingDuration = .33f;
+
+    private Vector3 startPosition;
+    private Vector3 startRotation;
+    private HandState state;
+    private bool shiftBack = false;
+    private Vector3 adjustRotation;
+    private float rotationDuration = 2;
+
+    private float countDown = 0;
+
+	// Use this for initialization
+	void Start () {
+        state = HandState.Start;
+        startPosition = transform.position;
+        startRotation = transform.rotation.eulerAngles;
+        Debug.Log("StartPosition: " + startPosition);
+	}
+
+    public void Swing()
+    {
+        ResetHand();
+        state = HandState.Swinging;
+        countDown = swingDuration;
+    }
+
+    private void HandleSwinging() {
+        countDown -= Time.deltaTime;
+        if (countDown <= 0)
+        {
+            ResetHand();
+            state = HandState.Start;
+            countDown = Random.Range(intervalMin, intervalMax);
+            return;
+        }
+        float swingRot = swingRotSpeed * Time.deltaTime;
+        transform.Rotate(new Vector3(0, swingRot, 0));
+
+    }
+
+    private void SetNextAdjust()
+    {
+        countDown = Random.Range(intervalMin, intervalMax);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (state == HandState.Start) {
+            HandleStartState();
+        } else if (state == HandState.Adjusting) {
+            HandleAdjustingState();
+        } else if (state == HandState.FinishedAdjusting) {
+            HandleAdjustFinishedState();
+        } else if (state == HandState.Reversing) {
+            HandleReversingState();
+        } else if (state == HandState.Swinging) {
+            HandleSwinging();
+        }
+	}
+
+    private void HandleStartState()
+    {
+        countDown -= Time.deltaTime;
+        if (countDown <= 0)
+        {
+            StartAdjustingHand();
+            countDown = Random.Range(durationMin, durationMax);
+            NextState();
+            rotationDuration = countDown;
+        }
+    }
+
+    private void HandleAdjustingState()
+    {
+        float rotationTime = Time.deltaTime;
+        if (Time.deltaTime >= countDown)
+        {
+            rotationTime = countDown;
+        }
+        transform.Rotate(adjustRotation * rotationTime / rotationDuration);
+        countDown -= Time.deltaTime;
+        if (countDown <= 0)
+        {
+            countDown = Random.Range(durationMin, durationMax);
+            NextState();
+        }
+    }
+
+    public void ResetHand()
+    {
+        Vector3 rot = transform.parent.rotation.eulerAngles + startRotation;
+        transform.rotation = Quaternion.Euler(rot);
+        // transform.position = startPosition;
+        Debug.Log("new Pos: " + transform.position);
+    }
+
+    private void HandleAdjustFinishedState()
+    {
+        countDown -= Time.deltaTime;
+        if (countDown <= 0)
+        {
+            countDown = Random.Range(durationMin, durationMax);
+            rotationDuration = countDown;
+            NextState();
+        }
+    }
+
+    private void HandleReversingState()
+    {
+        float rotationTime = Time.deltaTime;
+        if (Time.deltaTime >= countDown)
+        {
+            rotationTime = countDown;
+        }
+        transform.Rotate(-adjustRotation * rotationTime / rotationDuration);
+        countDown -= Time.deltaTime;
+        if (countDown <= 0)
+        {
+            countDown = Random.Range(intervalMin, intervalMax);
+            NextState();
+        }
+    }
+
+    private void StartAdjustingHand()
+    {
+        float rotX = Random.Range(xRotMin, xRotMax);
+        float rotY = Random.Range(yRotMin, yRotMax);
+        float rotZ = Random.Range(zRotMin, zRotMax);
+        adjustRotation = new Vector3(rotX, rotY, rotZ);
+    }
+
+    private float NextState()
+    {
+        if (state == HandState.Start) {
+            state = HandState.Adjusting;
+        } else if (state == HandState.Adjusting) {
+            state = HandState.FinishedAdjusting;
+        } else if (state == HandState.FinishedAdjusting) {
+            state = HandState.Reversing;
+        } else if (state == HandState.Reversing) {
+            state = HandState.Start;
+        }
+        Debug.Log("Next State: " + this.state);
+        return countDown;
+    }
+}
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public GameObject playerModel;
+    public Transform camera;
+    public float rightMultiplier = 1;
+    public float forwardMultiplier = 1;
+    public float walkSpeed = 4;
+    public float runSpeed = 8;
+
+    public float cameraX = 1;
+    public float cameraY = 1;
+    public float cameraSensitivity = 25;
+
+	// Use this for initialization
+	void Start () {
+        playerModel = gameObject;
+        camera = transform.Find("CameraHolder");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        MoveCharacter();
+        MoveCamera();
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("main fire!");
+            Transform hand = camera.Find("RightHand");
+            ActivateHand(hand);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("alt fire!");
+            Transform hand = camera.Find("LeftHand");
+            ActivateHand(hand);
+        }
+	}
+    
+    private void ActivateHand(Transform handTrans) {
+        if (handTrans != null)
+        {
+            HandMovement hand = handTrans.gameObject.GetComponent<HandMovement>();
+            hand.Swing();
+        }
+    }
+
+    private void MoveCharacter()
+    {
+        float right = Input.GetAxis("Horizontal") * rightMultiplier;
+        float forward = Input.GetAxis("Vertical") * forwardMultiplier;
+        Vector3 move = new Vector3(right, 0, forward) * Time.deltaTime * walkSpeed;
+        //playerModel.GetComponent<Rigidbody>().AddForce(move);
+        playerModel.transform.Translate(move);
+    }
+
+    private void MoveCamera()
+    {
+        float rotY = Input.GetAxis("Mouse X") * cameraX* Time.deltaTime * cameraSensitivity;
+        float rotX = Input.GetAxis("Mouse Y") * cameraY* Time.deltaTime * cameraSensitivity;
+        playerModel.transform.Rotate(new Vector3(0, rotY, 0));
+
+        camera.Rotate(new Vector3(-rotX, 0, 0));
+    }
+}
